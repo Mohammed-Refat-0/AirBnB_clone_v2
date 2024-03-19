87c58e307f007e007482d0b3c339e4ca6d4f15b5
Mohammed Refat <mohammedrefat53@outlook.com>
2024-03-19 23:24:08 +0200

    second upload

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,4 @@
 -- Prepares a MySQL Test server for the project.
---- create a usera and grants privileges
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
