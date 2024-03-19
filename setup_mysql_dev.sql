914615d005335d10b34e00dfed7f32b9df4e8e5b
Mohammed Refat <mohammedrefat53@outlook.com>
2024-03-19 23:26:27 +0200

    third upload

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,4 @@
--- script that prepares a MySQL server by creating a user and granting privileges.
+-- prepares a MySQL server by creating a user and granting privileges.
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGE ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
