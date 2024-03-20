a8443a7eb5f484f579a5eda56c07af3b3c20b5f8
Mohammed Refat <mohammedrefat53@outlook.com>
2024-03-20 20:35:09 +0200

    users_fix

--- models/user.py
+++ models/user.py
@@ -2,10 +2,6 @@
 """This is the user class implementation"""
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
-from models.place import Place
-from models.review import Review
 
 
 class User(BaseModel, Base):
@@ -19,5 +15,5 @@ class User(BaseModel, Base):
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
