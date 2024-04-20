e2e620b3bc96e90a6f028875fb5b84625ffc499e
Mohammed Refat <mohammedrefat53@outlook.com>
2024-04-20 18:31:01 +0200

    task 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" dynamic content configuration on
+server listening on 0.0.0.0, port 5000"""
+
+from flask import Flask
+
+app = Flask(__name__)
+app.__strict_slashes__ = False
+
+
+@app.route('/')
+def hello():
+    """return "Hello HBNB!" """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def HBNB():
+    """HBNB!" """
+    return 'HBNB!'
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
