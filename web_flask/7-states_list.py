71e0a58761ac93ec8f1a31baac2d126a1c6aa98a
Mohammed Refat <mohammedrefat53@outlook.com>
2024-04-21 21:02:42 +0200

    task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""flask webframe for lists of states in AiBnB"""
+from flask import Flask
+from flask import render_template
+from models import storage
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/states_list')
+def states_list():
+    """list all states saved in a instance of dbstorage"""
+    states_dict = storage.all("State")
+    return render_template('7-states_list.html', states=states_dict)
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
